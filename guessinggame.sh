c2182b4bf7bc90c5d89471f1e50ae58fed29be80
iagouar <panerasj@gmail.com>
2018-02-24 10:21:32 -0500

    added guessinggame.sh

---
+++ guessinggame.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+# File: guessinggame.sh
+
+
+#this command used to exclude any sub-directories that may be in the current directory
+file_num () {
+	ls -l | egrep -v "^total|^d" | wc -l
+}
+
+read -p "Guess number of files: " guessnum
+
+while [[ $guessnum -ne $(file_num) ]] 
+do {
+echo "You entered $guessnum"
+
+if [[ $guessnum -gt $(file_num) ]] 
+then
+	echo "Sorry, too high"
+	read -p "Guess again: " guessnum
+elif [[ $guessnum -lt $(file_num) ]]
+then	
+	echo "Sorry, too low"
+	read -p "Guess again: " guessnum
+fi	
+}
+done
+
+echo "You guessed right, way to go!"
